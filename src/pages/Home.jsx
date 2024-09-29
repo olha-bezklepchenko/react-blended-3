4ea684665bf707e3e691ea22b9debfb24a25dd88
Olha Bezklepchenko <krolichek.11@gmail.com>
2024-09-29 14:17:19 +0200

    added homePage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,34 @@
-import { Container, Heading, Section } from 'components';
+import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { getCountries } from 'service/countryApi';
 
 export const Home = () => {
+  const [countries, setCountries] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const data = await getCountries();
+        setCountries(data);
+      } catch (error) {
+        console.log(error);
+        setIsError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <Section>
       <Container>
-        <Heading title="Home" bottom />
+        {isLoading && <Loader />}
+        <CountryList countries={countries} />
+        {isError && <Heading title="Something is wrong..." bottom />}
       </Container>
     </Section>
   );
