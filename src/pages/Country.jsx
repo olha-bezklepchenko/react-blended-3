087436c85f35843d0e31d298ea88283f86e3a780
Yarik2019 <yroslavterno@gmail.com>
2024-09-29 17:10:26 +0300

    add coutry

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,45 @@
-import { Container, Heading, Section } from 'components';
+import {
+  Container,
+  Heading,
+  Section,
+  CountryInfo,
+  Loader,
+  GoBackBtn,
+} from 'components';
+import { useEffect, useRef, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import { fetchCountry } from 'service/countryApi';
 
 export const Country = () => {
+  const { countryId } = useParams();
+  const [country, setCountry] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const location = useLocation();
+  const goBackLink = useRef(location.state || '/');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const data = await fetchCountry(countryId);
+
+        setCountry(data);
+      } catch (error) {
+        setErrorMessage(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [countryId]);
   return (
     <Section>
       <Container>
-        <Heading title="SearchCountry" bottom />
+        <GoBackBtn path={goBackLink.current} />
+        {country && <CountryInfo {...country} />}
+        {loading && <Loader />}
+        {errorMessage && <Heading title="Something is wrong..." bottom />}
       </Container>
     </Section>
   );
